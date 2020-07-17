269a6884f1111acdea4cb30a5a65ae189fd85962
gfsalgado1 <52936092+gfsalgado1@users.noreply.github.com>
2020-07-17 10:11:22 -0500

    FIRST COMMIT
    Flashlight swift code project

---
+++ Flashlight/Flashlight/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Flashlight
+//
+//  Created by user on 7/9/20.
+//  Copyright © 2020 user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+ //   @IBOutlet weak var greenLabel: UILabel!
+
+    @IBAction func onButtonTap(_ sender: UIButton) {
+        self.view.backgroundColor = UIColor.blue
+   //     greenLabel.textColor = UIColor.green
+    //    self.greenLabel.text = "It is not easy being green"
+    }
+    
+   
+}
+
